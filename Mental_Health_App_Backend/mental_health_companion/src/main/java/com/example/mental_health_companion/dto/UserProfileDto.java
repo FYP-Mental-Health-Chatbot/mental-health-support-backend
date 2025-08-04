1a6c36d36a6d4e7fe6404728d9b3b0d04a6867e0
sulaiman4342 <sulaimannuhash@gmail.com>
2025-08-04 16:57:19 +0530

    Create seperate dto for user profile

---
+++ Mental_Health_App_Backend/mental_health_companion/src/main/java/com/example/mental_health_companion/dto/UserProfileDto.java
@@ -0,0 +1,14 @@
+package com.example.mental_health_companion.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProfileDto {
+    private String fullName;
+    private String email;
+}
